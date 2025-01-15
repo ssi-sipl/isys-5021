2a8acb3eb3d7e0df8297201cbf4ad8bd24abda97
abhinnvyas <abhinnvyas2@gmail.com>
2025-01-15 17:24:24 +0530

    changes

--- ISYS_5021_150M_POSITION.py
+++ ISYS_5021_150M_POSITION.py
@@ -199,8 +199,8 @@ def parse_data_packet(data, frame_id):
         print(f"{'Serial':<8} {'Signal Strength (dB)':<25} {'Range (m)':<15} {'Velocity (m/s)':<25} {'Direction':<15} {'Azimuth (Deg)':<25} {'x (m) y (m)':<25} {'Latitude':<25} {'Longitude':<25}")
         print("-" * 150)
         for idx, target in enumerate(targets, start=1):
-            direction = "Static" if target["velocity"] == 0 else "Incomming" if target["velocity"] > 0 else "Outgoing"
-            print(f"{idx:<8} {target['signal_strength']:<25} {target['range']:<15} {target['velocity']:<25} {direction:<15} {target['azimuth']:<25} {target['x']} {target['y']:<25} {target['latitude']:<25} {target['longitude']:<25}")
+            direction = "Static" if target["speed"] == 0 else "Incomming" if target["speed"] > 0 else "Outgoing"
+            print(f"{idx:<8} {target['signal_strength']:<25} {target['range']:<15} {target['speed']:<25} {direction:<15} {target['aizmuth_angle']:<25} {target['x']} {target['y']:<25} {target['latitude']:<25} {target['longitude']:<25}")
                     
         print("-" * 50)
 

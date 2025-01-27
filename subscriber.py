27ca22cc9312b4ea2ffb72ef9309877148cf084b
abhinnvyas <abhinnvyas2@gmail.com>
2025-01-27 12:59:12 +0530

    made the code easier to use

--- subscriber.py
+++ subscriber.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqtt
-
-MQTT_CHANNEL = "radar_surveillance"
+from config import *
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected to broker with result code {rc}")
@@ -14,7 +13,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect("192.168.1.53", 1883, 60) # ip to the rpi4
+    client.connect(MQTT_BROKER_SUBSCRIBER, MQTT_PORT, 60) # ip to the rpi4
     client.loop_start()
 
     try:
